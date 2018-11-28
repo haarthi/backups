d2d24e495cb1b40dfd1bb4173a0a37fb844c90de
haarthi <hsadasivam@gmail.com>
2018-11-28 09:50:17 -0500

    my message8

--- demo-gzr.rb
+++ demo-gzr.rb
@@ -47,9 +47,9 @@ end
     print g.status.changed
     print "there is a diff"
     g.add(:all=>true)    
-    print g.commit("my message8")
+    g.commit("my message8")
 
-    system("g.push")
+    system("git push")
 
   else 
     print "no diff"
